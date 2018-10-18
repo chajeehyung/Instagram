338286d98f601ab5c06fe3325d1edaa4186f1919
chajeehyung <chajee308@gmail.com>
2018-10-18 12:02:03 +0900

    모델 및 어플리케이션 한국어화

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,34 +1,42 @@
 from django.db import models
 
-
 class Post(models.Model):
     author = models.ForeignKey(
         #<AppName,<ModelName> 임포트해서도 해도 된
         'members.User',
         on_delete=models.CASCADE,
+        verbose_name='작성자'
     )
 
-    photo = models.ImageField(upload_to='post')
+    photo = models.ImageField('사진',upload_to='post')
+
 
 
 class Comment(models.Model):
     post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE,
+        verbose_name='포스트'
     )
     author = models.ForeignKey(
         'members.User',
         on_delete=models.CASCADE,
+        verbose_name='작성자'
     )
-    content = models.TextField()
+    content = models.TextField('댓글내용')
     tags = models.ManyToManyField(
         'HashTag',
         blank=True,
+        verbose_name='해시태그 목록'
     )
+
+    class Meta:
+        verbose_name = '댓글'
+        verbose_name_plural = f'{verbose_name} 목록'
 
 
 class HashTag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField('태그명', max_length=100)
 
     def __str__(self):
         return self.name
