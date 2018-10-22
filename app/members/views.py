e993daa18380b825ad7be87dddc93a765cb947b2
chajeehyung <chajee308@gmail.com>
2018-10-22 15:49:12 +0900

    로그아웃 로그인 기능 구현

--- app/members/views.py
+++ app/members/views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
-from members.forms import LoginForm
+from members.forms import LoginForm, SignupForm
 
 
 def login_view(request):
@@ -9,12 +9,12 @@ def login_view(request):
         # 1. request.POST에 데이터가 옴
         # 2. 온 데이터 중에서 username에 해당하는 값과 password에 해당하는 값을  각각
         #   username, password변수에 할당
-        #3. 사용자 인증을 수행
+        # 3. 사용자 인증을 수행
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user is not None:
-            #인증 성공시
+            # 인증 성공시
             login(request, user)
             return redirect('posts:post-list')
         else:
@@ -28,3 +28,29 @@ def login_view(request):
         }
 
         return render(request, 'members/login.html', context)
+
+
+def logout_view(request):
+    # URL: /members/logout/
+    # Template: 없음
+
+    # !POST요청일 때만 처리
+    # 처리 완료 후 'posts:post-list'로 이동
+
+    # base.html에 있는 'Logout'버튼이 이 view로의 POST요청을 하도록 함
+    #  -> form을 구현해야 함
+    #      'action'속성의 값을 이 view로
+    if request.method == 'POST':
+        logout(request)
+        return redirect('posts:post-list')
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = SignupForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'members/signup.html', context)
