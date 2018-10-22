6732a6b2c819a39295a7f1b26343459d91caac8e
chajeehyung <chajee308@gmail.com>
2018-10-22 16:31:12 +0900

    username, password1, passwor2가 유효한 경우의 회원가입 기능 구현

--- app/members/views.py
+++ app/members/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from members.forms import LoginForm, SignupForm
@@ -47,6 +49,33 @@ def logout_view(request):
 
 def signup_view(request):
     if request.method == 'POST':
+        # 1. request.POST에 전달된 username, password1, password2를 각각 해당 이름의 변수에 할당
+        # 2. -x에서는 HttpResponse에 문자열로 에러를 리턴해주기
+        #   2-1 username에 해당하는 User가 이미 있다면
+        #       사용자명 ({ usernama }) 이미 사용중입니다
+        #   2-2 password1과 password2가 일치하지 않는다면
+        #       비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다.
+        # 3. 위의 두 경우가 아니라면
+        #     새 User를 생성, 해당 User로 로그인 시켜준 후 'posts:post-list'로 redirect 처
+
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if User.objects.filter(username=username).exists():
+            return HttpResponse(f'사용자면({username})은 이미 사용중입니다.')
+        if password1 != password2:
+            return HttpResponse('비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다.')
+
+        # create_user 메서드는 create와 달리 자동으로 해싱해줌
+        user = User.objects.create_user(
+            username=username,
+            password=password1,
+        )
+        login(request, user)
+        return redirect('posts:post-list')
+
+
         pass
     else:
         form = SignupForm()
